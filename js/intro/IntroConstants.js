f5eb8a015bce31bbc8718b7041190e4bb721c6ae
coledu <coledu@berea.edu>
2017-05-12 14:21:42 -0400

    Create function to make ticks on number line.

--- js/intro/IntroConstants.js
+++ js/intro/IntroConstants.js
@@ -17,8 +17,8 @@ define( function( require ) {
     // Constants for number line
     NUMBER_LINE_WIDTH: 800, // Width used for number line.
     MAX_NUMBER_OF_UNITS_RANGE: new RangeWithValue( 1, 6, 1 ),
-    MAJOR_TICK_LENGTH: 50, //length of major tick line
-    MINOR_TICK_LENGTH: 25, //length of minor tick line
+    MAJOR_TICK_LENGTH: 100, //length of major tick line
+    MINOR_TICK_LENGTH: 50, //length of minor tick line
     DENOMINATOR_RANGE: new RangeWithValue( 1, 8, 1 ),
     NUMBER_LINE_FONT: new PhetFont( 50 )
   } );
