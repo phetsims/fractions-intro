e14043ff00a9271f7b8add054f82245a1b312484
coledu <coledu@berea.edu>
2017-05-12 09:26:02 -0400

    use path and shape to layout ticks. (see #3)

--- js/intro/view/NumberLineNode.js
+++ js/intro/view/NumberLineNode.js
@@ -11,10 +11,12 @@ define( function( require ) {
   // modules
   var fractionsIntro = require( 'FRACTIONS_INTRO/fractionsIntro' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var IntroConstants = require( 'FRACTIONS_INTRO/intro/IntroConstants' );
   var Line = require( 'SCENERY/nodes/Line' );
+  var Node = require( 'SCENERY/nodes/Node' );
+  var Path = require( 'SCENERY/nodes/Path' );
   var Property = require( 'AXON/Property' );
+  var Shape = require( 'KITE/Shape' );
 
   /**
    * @param {Property.<number>} maxNumberOfUnitsProperty
@@ -29,13 +31,17 @@ define( function( require ) {
     var mainNumberLine = new Line( 0, 0, IntroConstants.NUMBER_LINE_WIDTH, 0, { stroke: 'black' } );
     this.addChild( mainNumberLine );
 
-    // Major Ticks
-    var majorTicksNode = new Node();
-    this.addChild( majorTicksNode );
+    // Even Major Ticks, Width of line is slightly thicker than the odd Major Ticks
+    var evenMajorTicksPath = new Path( null, { stroke: 'black', lineWidth: 5 } );
+    this.addChild( evenMajorTicksPath );
+
+    // odd Major Ticks
+    var oddMajorTicksPath = new Path( null, { stroke: 'black', lineWidth: 3 } );
+    this.addChild( oddMajorTicksPath );
 
     // Minor Ticks
-    var minorTicksNode = new Node();
-    this.addChild( minorTicksNode );
+    var minorTicksPath = new Path( null, { stroke: 'black', lineWidth: 1 } );
+    this.addChild( minorTicksPath );
 
     // Present for the lifetime of the simulation
     // Updates the minor and major ticks as well as the main number line
@@ -45,29 +51,34 @@ define( function( require ) {
       // sets the length of the main number line
       mainNumberLine.setX2( segmentLength * maxNumberOfUnits );
 
-      // lays out the major ticks
-      majorTicksNode.removeAllChildren();
+      // create major ticks shape
+      var evenMajorTicksShape = new Shape();
+      var oddMajorTicksShape = new Shape();
+
       for ( var i = 0; i <= maxNumberOfUnits; i++ ) {
 
         // major tick line width varies for even and odd number of units
-        var majorTickLineWidth = (i % 2) ? 3 : 5;
-        var majorTickLine = new Line( i * segmentLength, -IntroConstants.MAJOR_TICK_LENGTH, i * segmentLength, IntroConstants.MAJOR_TICK_LENGTH,
-          { stroke: 'black', lineWidth: majorTickLineWidth } );
-        majorTicksNode.addChild( majorTickLine );
+        if ( i % 2 === 0 ) {
+          evenMajorTicksShape.moveTo( i * segmentLength, -IntroConstants.MAJOR_TICK_LENGTH ).verticalLineToRelative( 2 * IntroConstants.MAJOR_TICK_LENGTH );
+        }
+        else {
+          oddMajorTicksShape.moveTo( i * segmentLength, -IntroConstants.MAJOR_TICK_LENGTH ).verticalLineToRelative( 2 * IntroConstants.MAJOR_TICK_LENGTH );
+        }
       }
+      evenMajorTicksPath.setShape( evenMajorTicksShape );
+      oddMajorTicksPath.setShape( oddMajorTicksShape );
 
       // lays out the minor ticks
+      var minorTicksShape = new Shape();
       var minorTickSeparation = segmentLength / denominator;
-      minorTicksNode.removeAllChildren();
       for ( var j = 0; j <= maxNumberOfUnits * denominator; j++ ) {
 
         // skips major tick lines
         if ( j % denominator !== 0 ) {
-          var minorTickLine = new Line( j * minorTickSeparation, -IntroConstants.MINOR_TICK_LENGTH, j * minorTickSeparation,
-            IntroConstants.MINOR_TICK_LENGTH, { stroke: 'black' } );
-          minorTicksNode.addChild( minorTickLine );
+          minorTicksShape.moveTo( j * minorTickSeparation, -IntroConstants.MINOR_TICK_LENGTH ).verticalLineToRelative( 2 * IntroConstants.MINOR_TICK_LENGTH );
         }
       }
+      minorTicksPath.setShape( minorTicksShape );
     } );
     this.mutate( options );
   }
