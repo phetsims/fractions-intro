0ebd250a8f2fafe41d1ff9ae669e4d50433e244a
michaelm396 <michael.moorer396@gmail.com>
2017-05-24 11:23:38 -0400

    fix bug in FractionNode.j that prevented options.iteractive from working properly

--- js/intro/view/FractionNode.js
+++ js/intro/view/FractionNode.js
@@ -33,6 +33,9 @@ define( function( require ) {
     options = _.extend( {
       fill: 'black',
       font: new PhetFont( { size: 84 } ),
+      dividingLineLength: 80,
+      dividingLineWidth: 4,
+
       // By default the fraction node is interactive, which means it has up/down spinners
       // Those spinners can be removed if the fraction node will be used as a label for underneath the number line
       interactive: true
@@ -41,7 +44,8 @@ define( function( require ) {
     var numeratorNode = new Text( numeratorProperty.get(), { font: options.font, fill: options.fill } );
 
     // creates a division line beneath numerator
-    var line = new Line( 0, 0, 80, 0, { lineWidth: 4, lineCap: 'round', stroke: options.fill } );
+    var line = new Line( 0, 0, options.dividingLineLength, 0, {
+      lineWidth: options.dividingLineWidth, lineCap: 'round', stroke: options.fill } );
 
     // centers the numeratorNode horizontally between the division line
     numeratorProperty.link( function( value ) {
@@ -90,8 +94,12 @@ define( function( require ) {
       } );
     }
 
+
     // Specify the children to be rendered with this node
-    options.children = [ line, numeratorNode, denominatorNode, spinnerVBox ];
+    options.children = [ line, numeratorNode, denominatorNode];
+    if (options.interactive ){
+      options.children.push( spinnerVBox );
+    }
     Node.call( this, options );
   }
 
