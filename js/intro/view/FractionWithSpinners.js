8def575a922c445b3cc6eb6be5f149a19398e734
Vincent Davis <truckerman1910@gmail.com>
2017-06-12 14:39:23 -0400

    use an HBox to position spinners with fraction (see #47)

--- js/intro/view/FractionWithSpinners.js
+++ js/intro/view/FractionWithSpinners.js
@@ -13,14 +13,11 @@ define( function( require ) {
   var fractionsIntro = require( 'FRACTIONS_INTRO/fractionsIntro' );
   var FractionNode = require( 'FRACTIONS_INTRO/intro/view/FractionNode' );
   var DerivedProperty = require( 'AXON/DerivedProperty' );
+  var HBox = require( 'SCENERY/nodes/HBox' );
   var IntroConstants = require( 'FRACTIONS_INTRO/intro/IntroConstants' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var NumberSpinner = require( 'SUN/NumberSpinner' );
-  var Node = require( 'SCENERY/nodes/Node' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
-  var Property = require( 'AXON/Property' );
   var UpDownSpinner = require( 'SCENERY_PHET/UpDownSpinner' );
-  var RangeWithValue = require( 'DOT/RangeWithValue' );
   var VBox = require( 'SCENERY/nodes/VBox' );
 
   /**
@@ -39,12 +36,11 @@ define( function( require ) {
       dividingLineWidth: 10
     }, options );
 
-    var fractionNode = new FractionNode( numeratorProperty, denominatorProperty );
-
     // Enables or Disables Spinners as dependent on numeratorProperty, denominatorProperty, or maxProperty
     var numeratorUpEnabledProperty = new DerivedProperty(
       [ numeratorProperty, denominatorProperty, maxProperty ],
       function( numerator, denominator, max ) { return numerator < denominator * max; } );
+
     var numeratorDownEnabledProperty = new DerivedProperty(
       [ numeratorProperty ],
       function( numerator ) { return numerator > 0; } );
@@ -54,28 +50,27 @@ define( function( require ) {
     var denominatorDownEnabledProperty = new DerivedProperty(
       [ numeratorProperty, denominatorProperty, maxProperty ],
       function( numerator, denominator, max ) { return denominator > IntroConstants.DENOMINATOR_RANGE.min && numerator <= (denominator - 1) * max;} );
-
-
     // creates spinner that is linked to the numeratorProperty
     var numeratorSpinner = new UpDownSpinner( numeratorProperty, numeratorUpEnabledProperty, numeratorDownEnabledProperty );
 
     // creates spinner that is linked to the denominatorProperty
     var denominatorSpinner = new UpDownSpinner( denominatorProperty, denominatorUpEnabledProperty, denominatorDownEnabledProperty );
 
-    // Aligns the numeratorSpinner and denominatorSpinner vertically
-    var spinnerVBox = new VBox( {
-      spacing: 50,
-      children: [ numeratorSpinner, denominatorSpinner ],
-      right: fractionNode.left,
-      centerY: fractionNode.centerY
-    } );
+    var fractionNode = new FractionNode( numeratorProperty, denominatorProperty );
 
     // Specify the children to be rendered with this node
-    options.children = [ fractionNode, spinnerVBox ];
-    Node.call( this, options );
+    HBox.call( this, _.extend( options, {
+      children: [
+        new VBox( {
+          spacing: 50,
+          children: [ numeratorSpinner, denominatorSpinner ]
+        } ),
+        fractionNode ]
+    } ) );
+
   }
 
   fractionsIntro.register( 'FractionWithSpinners', FractionWithSpinners );
 
-  return inherit( Node, FractionWithSpinners );
+  return inherit( HBox, FractionWithSpinners );
 } );
