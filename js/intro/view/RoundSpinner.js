fbb818fe85ef71001b72a615ba6282d947108161
coledu <coledu@berea.edu>
2017-06-13 14:55:59 -0400

    add horizontal orientation to the spinner

--- js/intro/view/RoundSpinner.js
+++ js/intro/view/RoundSpinner.js
@@ -35,50 +35,80 @@ define( function( require ) {
         valueProperty.set( valueProperty.get() - 1 );
       },
       fireOnHold: false,
-      orientation: 'vertical',
+      orientation: 'horizontal',
       buttonOrientation: 'vertical',
       iconScale: 1.3,
-      radius: 20
+      radius: 20,
+      spacing: 6
     }, options );
 
     var shapeWidth = options.radius * options.iconScale;
-    var upShape = new Shape().moveTo( 0, 0 ).lineTo( shapeWidth / 2, -10 * options.radius / 20 ).lineTo( shapeWidth, 0 );
-    var downShape = new Shape().moveTo( 0, 0 ).lineTo( shapeWidth / 2, 10 * options.radius / 20 ).lineTo( shapeWidth, 0 );
 
-    var upIcon = new Path( upShape, { lineWidth: 5 * options.radius / 20, stroke: 'black', lineCap: 'round' } );
-    var downIcon = new Path( downShape, { lineWidth: 5 * options.radius / 20, stroke: 'black', lineCap: 'round' } );
+    var arrowShape = new Shape().moveTo( 0, 0 ).lineTo( shapeWidth / 2, -10 * options.radius / 20 ).lineTo( shapeWidth, 0 );
 
-    var upButton = new RoundPushButton( {
-      content: upIcon,
+    var incrementIcon = new Path( arrowShape, {
+      lineWidth: 5 * options.radius / 20,
+      stroke: 'black',
+      lineCap: 'round'
+    } );
+
+    var decrementIcon = new Path( arrowShape, {
+      lineWidth: 5 * options.radius / 20,
+      stroke: 'black',
+      lineCap: 'round'
+    } );
+    decrementIcon.rotateAround( decrementIcon.center, Math.PI );
+
+    // Adjust the the arrow on the button for the correct orientation
+    if ( options.orientation === 'horizontal' ) {
+      incrementIcon.rotateAround( incrementIcon.center, Math.PI / 2 );
+      decrementIcon.rotateAround( decrementIcon.center, Math.PI / 2 );
+      var xContentOffset = -3;
+      var yContentOffset = 0;
+    }
+    else {
+      yContentOffset = -3;
+      xContentOffset = 0;
+
+    }
+
+    var incrementButton = new RoundPushButton( {
+      content: incrementIcon,
       listener: options.upButtonListener,
       radius: options.radius,
       touchAreaDilation: 5,
       baseColor: '#fefd53',
-      yContentOffset: -3,
+      yContentOffset: (options.orientation === 'horizontal') ? -yContentOffset : yContentOffset,
+      xContentOffset: -xContentOffset,
 
       // options related to fire-on-hold feature
       fireOnHold: options.fireOnHold,
       fireOnHoldDelay: 400, // start to fire continuously after pressing for this long (milliseconds)
       fireOnHoldInterval: 200 // fire continuously at this interval (milliseconds)
     } );
-    upEnabledProperty.linkAttribute( upButton, 'enabled' );
 
-    var downButton = new RoundPushButton( {
-      content: downIcon,
+    var decrementButton = new RoundPushButton( {
+      content: decrementIcon,
       listener: options.downButtonListener,
       radius: options.radius,
       touchAreaDilation: 5,
       baseColor: '#fefd53',
-      yContentOffset: +3,
+      yContentOffset: (options.orientation === 'horizontal') ? yContentOffset : -yContentOffset,
+      xContentOffset: xContentOffset,
 
       // options related to fire-on-hold feature
       fireOnHold: options.fireOnHold,
       fireOnHoldDelay: 400, // start to fire continuously after pressing for this long (milliseconds)
       fireOnHoldInterval: 200 // fire continuously at this interval (milliseconds)
     } );
-    downEnabledProperty.linkAttribute( downButton, 'enabled' );
 
-    LayoutBox.call( this, { spacing: 6, children: [ upButton, downButton ] } );
+    upEnabledProperty.linkAttribute( incrementButton, 'enabled' );
+
+    downEnabledProperty.linkAttribute( decrementButton, 'enabled' );
+
+    var orderArray = (options.orientation === 'horizontal') ? [ decrementButton, incrementButton ] : [ incrementButton, decrementButton ];
+
+    LayoutBox.call( this, { spacing: options.spacing, children: orderArray } );
 
     this.mutate( options );
   }
