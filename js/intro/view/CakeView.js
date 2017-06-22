b23fb407178069a66f3ed922581ca2608a762ad7
Martin Veillette <martin.veillette@gmail.com>
2017-06-21 21:36:55 -0400

    add JSDoc to CakeView

--- js/intro/view/CakeView.js
+++ js/intro/view/CakeView.js
@@ -1,7 +1,7 @@
 // Copyright 2017, University of Colorado Boulder
 
 /**
- * TODO: doc
+ * Scenery Node for the container set of cakes with a bucket
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -11,9 +11,9 @@ define( function( require ) {
   // modules
   var CakeContainerNode = require( 'FRACTIONS_INTRO/intro/view/CakeContainerNode' );
   var CakePieceNode = require( 'FRACTIONS_INTRO/intro/view/CakePieceNode' );
+  var CellSceneView = require( 'FRACTIONS_INTRO/intro/view/CellSceneView' );
   var fractionsIntro = require( 'FRACTIONS_INTRO/fractionsIntro' );
   var inherit = require( 'PHET_CORE/inherit' );
-  var CellSceneView = require( 'FRACTIONS_INTRO/intro/view/CellSceneView' );
 
   /**
    * @constructor
@@ -28,10 +28,25 @@ define( function( require ) {
   fractionsIntro.register( 'CakeView', CakeView );
 
   return inherit( CellSceneView, CakeView, {
+    /**
+     * create a cake container that holds a cake plate and the cakeNodes
+     * @param {Container} container
+     * @param {Function} cellDownCallback
+     * @returns {CakeContainerNode}
+     * @private
+     */
     createContainerNode: function( container, cellDownCallback ) {
       return new CakeContainerNode( container, cellDownCallback );
     },
 
+    /**
+     * create a cake piece node
+     * @param {Piece} piece
+     * @param {Function} finishedAnimatingCallback
+     * @param {Function} droppedCallback
+     * @returns {CakePieceNode}
+     * @private
+     */
     createPieceNode: function( piece, finishedAnimatingCallback, droppedCallback ) {
       return new CakePieceNode( piece, finishedAnimatingCallback, droppedCallback );
     }
